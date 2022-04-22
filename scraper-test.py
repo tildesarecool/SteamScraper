61ed6281914b8f5ef12fbb065a38588ec181ec2b
tildesarecool <tildesare.cool@gmail.com>
2022-04-21 22:48:04 -0700

    signifcant progress

--- scraper-test.py
+++ scraper-test.py
@@ -21,17 +21,42 @@ def RetrieveSteamData():
     PostambleStartPos = (scriptContent.find("= ") + 3)
     # jump to last character of JSON string (position)
     #startSuffixPos = scriptContent.find("rgChangingGames") - 8
-    startSuffixPos = scriptContent.find("rgChangingGames") - 10
+    startSuffixPos = scriptContent.find("rgChangingGames") - 11 # 10 is two '}' at the end
     # hopefully this is just the json string
     justGamedata = scriptContent[PostambleStartPos:startSuffixPos] 
+    #pyperclip.copy(justGamedata)
     return justGamedata
 #RetrieveSteamData()
 
-
+'''
 with open("oneline-singleentry.json", "r") as read_file: 
     data = json.load(read_file)
     #dtos = str(data)
     print(type(data))
     print(data)
     read_file.close()
+'''
+
+#test-entries.txt
+
+# hypothetical steps:
+# 1. bring in data from test-entries
+# 2. add pre- and post-amble
+# 3. use loads to convert to dictionary object first?...i think the dump() does need dictionary as input parameter
+# 4. used dump() to immediately write result to .json file
+
+with open('test-entries.txt', "r") as file:
+    contents = file.read()           # step 1: bring in data from txt file
+    #print(contents)
+    preamble = '''{ "game_library": ['''
+    postamble = ''']}'''
+    newContents = preamble + contents + postamble # step 2: add pre- and post-amble
+    #print(newContents)
+    newContentsAsJson = json.loads(newContents) # step 3: used loads() to convert data to dictionary object
+    print(type(newContentsAsJson))
+    print(newContentsAsJson)
+    file.close()
+
+with open('validated-data.json', "w") as jfile:
+    json.dump(newContentsAsJson, jfile, indent=4)
 
